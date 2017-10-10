4d6635e4e2c4a11a5779b9d389fe455d67afb140
Oliver Clark Rickard <ocrickard@gmail.com>
2017-10-10 15:44:36 -0400

    Setting up the workspace

---
+++ theodolite/theodolite.h
@@ -0,0 +1,19 @@
+//
+//  theodolite.h
+//  theodolite
+//
+//  Created by Oliver Rickard on 10/10/17.
+//  Copyright © 2017 Oliver Rickard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for theodolite.
+FOUNDATION_EXPORT double theodoliteVersionNumber;
+
+//! Project version string for theodolite.
+FOUNDATION_EXPORT const unsigned char theodoliteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <theodolite/PublicHeader.h>
+
+
