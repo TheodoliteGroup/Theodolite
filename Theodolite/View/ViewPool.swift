04edc84f3aa1303e09723f942a7f92b6d963d83d
Oliver Clark Rickard <ocrickard@gmail.com>
2017-10-11 21:29:40 -0400

    Move view pool map into its own file

--- Theodolite/View/ViewPool.swift
+++ Theodolite/View/ViewPool.swift
@@ -8,46 +8,6 @@
 
 import UIKit
 
-var kViewPoolMapKey: Void?;
-
-struct ViewPoolMap {
-  var hashMap: [ViewConfiguration:ViewPool] = [:];
-  
-  static func getViewPool(view: UIView, config: ViewConfiguration) -> ViewPool {
-    let map: ViewPoolMap? =
-      getAssociatedObject(
-        object: view,
-        associativeKey: &kViewPoolMapKey)
-    
-    var unwrapped = map ?? ViewPoolMap();
-    
-    if map == nil {
-      setAssociatedObject(object: view,
-                          value: map,
-                          associativeKey: &kViewPoolMapKey);
-    }
-    
-    if let pool = unwrapped.hashMap[config] {
-      return pool;
-    }
-    
-    let pool = ViewPool();
-    unwrapped.hashMap[config] = pool;
-    return pool;
-  }
-  
-  static func reset(view: UIView) {
-    if let map: ViewPoolMap =
-      getAssociatedObject(
-        object: view,
-        associativeKey: &kViewPoolMapKey){
-      for (_, var pool) in map.hashMap {
-        pool.reset();
-      }
-    }
-  }
-}
-
 struct ViewPool {
   var views: [UIView] = [];
   var iterator: Array<UIView>.Iterator;
---
+++ Theodolite/View/ViewPoolMap.swift
@@ -0,0 +1,49 @@
+//
+//  ViewPoolMap.swift
+//  Theodolite
+//
+//  Created by Oliver Rickard on 10/11/17.
+//  Copyright © 2017 Oliver Rickard. All rights reserved.
+//
+
+import Foundation
+
+var kViewPoolMapKey: Void?;
+
+struct ViewPoolMap {
+  var hashMap: [ViewConfiguration:ViewPool] = [:];
+  
+  static func getViewPool(view: UIView, config: ViewConfiguration) -> ViewPool {
+    let map: ViewPoolMap? =
+      getAssociatedObject(
+        object: view,
+        associativeKey: &kViewPoolMapKey)
+    
+    var unwrapped = map ?? ViewPoolMap();
+    
+    if map == nil {
+      setAssociatedObject(object: view,
+                          value: map,
+                          associativeKey: &kViewPoolMapKey);
+    }
+    
+    if let pool = unwrapped.hashMap[config] {
+      return pool;
+    }
+    
+    let pool = ViewPool();
+    unwrapped.hashMap[config] = pool;
+    return pool;
+  }
+  
+  static func reset(view: UIView) {
+    if let map: ViewPoolMap =
+      getAssociatedObject(
+        object: view,
+        associativeKey: &kViewPoolMapKey){
+      for (_, var pool) in map.hashMap {
+        pool.reset();
+      }
+    }
+  }
+}
