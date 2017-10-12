87fdc6788eba83c211096614fdbb936b75ad38a1
Oliver Clark Rickard <ocrickard@gmail.com>
2017-10-12 08:03:53 -0400

    Add tests for view configurations

--- TheodoliteTests/ViewConfigurationTests.swift
+++ TheodoliteTests/ViewConfigurationTests.swift
@@ -10,5 +10,103 @@ import XCTest
 @testable import Theodolite
 
 class ViewConfigurationTests: XCTestCase {
+  func test_whenBuildingViewConfiguration_withViewType_andNoAttributes_buildView_returnsViewType() {
+    let config = ViewConfiguration(view: UILabel.self, attributes: []);
+    let view = config.buildView();
+    XCTAssert(type(of: view) == UILabel.self);
+  }
   
+  func test_viewConfigurationWithSingleAttributes_buildView_appliesAttribute() {
+    let config = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.red) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    let view = config.buildView();
+    XCTAssert(view.backgroundColor == UIColor.red);
+  }
+  
+  func test_whenRebuildingView_withDifferentViewConfig_newAttributesApplied() {
+    let config = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.red) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    let view = config.buildView();
+    
+    let config2 = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.blue) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    config2.applyToView(v: view);
+    
+    XCTAssert(view.backgroundColor == UIColor.blue);
+  }
+  
+  func test_twoViewConfigurations_withIdenticalProperties_areEqual() {
+    let config = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.red) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    
+    let config2 = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.red) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    
+    XCTAssertEqual(config, config2);
+  }
+  
+  func test_twoViewConfigurations_withIdenticalProperties_haveIdenticalHashes() {
+    let config = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.red) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    
+    let config2 = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.red) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    
+    XCTAssertEqual(config.hashValue, config2.hashValue);
+  }
+  
+  func test_twoViewConfigurations_withDifferentProperties_areNotEqual() {
+    let config = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.red) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    
+    let config2 = ViewConfiguration(
+      view: UILabel.self,
+      attributes: [
+        Attr<UILabel, UIColor>(value: UIColor.blue) {(view: UILabel, val: UIColor) in
+          view.backgroundColor = val;
+        }
+      ]);
+    
+    XCTAssertNotEqual(config, config2);
+  }
 }
