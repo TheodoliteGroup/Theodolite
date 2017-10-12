ec897d7a3b3646fc1e62c3b29dd5e2dcbfb78c41
Oliver Clark Rickard <ocrickard@gmail.com>
2017-10-11 21:30:52 -0400

    Split attributes and configuration from view

---
+++ Theodolite/View/ViewConfiguration.swift
@@ -0,0 +1,39 @@
+//
+//  View.swift
+//  components-swift
+//
+//  Created by Oliver Rickard on 10/9/17.
+//  Copyright © 2017 Oliver Rickard. All rights reserved.
+//
+
+import UIKit
+
+struct ViewConfiguration: Equatable, Hashable {
+  let view: UIView.Type;
+  let attributes: [Attribute];
+  
+  public var hashValue: Int {
+    return self.attributes.reduce(self.view.hash()) {
+      (value: Int, attr: Attribute) -> Int in
+      return value ^ attr.hashValue;
+    };
+  }
+  
+  func applyToView(v: UIView) {
+    assert(type(of: v) == view);
+    for attr in self.attributes {
+      attr.apply(view: v);
+    }
+  }
+  
+  func buildView() -> UIView {
+    let v = self.view.init();
+    self.applyToView(v: v);
+    return v;
+  }
+}
+
+func ==(lhs: ViewConfiguration, rhs: ViewConfiguration) -> Bool {
+  return lhs.view === rhs.view
+    && lhs.attributes == rhs.attributes;
+}
