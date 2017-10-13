be762d07e347273b09444812e797ea5fa9628a38
Oliver Clark Rickard <ocrickard@gmail.com>
2017-10-13 09:04:30 -0400

    Add an optional extra param to allow passing data from layout to mount

--- Theodolite/Core/Layout.swift
+++ Theodolite/Core/Layout.swift
@@ -17,4 +17,25 @@ public struct Layout {
   let component: Component;
   let size: CGSize;
   let children: [LayoutChild];
+  
+  /* 
+   Can be used to pass data from layout to mount. Useful if you want to pre-compute something, and then use the
+   result.
+   
+   Canonical example is rendering text. You may want to pass the text artifacts from layout to mount so you don't have
+   to do the work of laying out the text again when mounting as an optimization.
+   
+   The framework will always leave this value nil. You can fill it with whatever you want.
+  */
+  let extra: Any?
+  
+  init(component: Component,
+       size: CGSize,
+       children: [LayoutChild],
+       extra: Any? = nil) {
+    self.component = component;
+    self.size = size;
+    self.children = children;
+    self.extra = extra;
+  }
 }
