ec45c1d905b9973a53c4b8bd5655d233aaf100d4
Oliver Clark Rickard <ocrickard@gmail.com>
2017-10-22 15:47:53 -0400

    Add component context

---
+++ TheodoliteTests/ContextTests.swift
@@ -0,0 +1,14 @@
+//
+//  ContextTests.swift
+//  TheodoliteTests
+//
+//  Created by Oliver Rickard on 10/22/17.
+//  Copyright © 2017 Oliver Rickard. All rights reserved.
+//
+
+import XCTest
+@testable import Theodolite
+
+class ContextTests: XCTestCase {
+
+}
