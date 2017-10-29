f53f58978593b6fbe70248f1f330054a2d65b9ad
Oliver Clark Rickard <ocrickard@gmail.com>
2017-10-28 23:19:17 -0400

    Add a generic tap component

---
+++ Theodolite/UI/Interaction/TapComponent.swift
@@ -0,0 +1,30 @@
+//
+//  TapComponent.swift
+//  Theodolite
+//
+//  Created by Oliver Rickard on 10/28/17.
+//  Copyright © 2017 Oliver Rickard. All rights reserved.
+//
+
+import UIKit
+
+public final class TapComponent: TypedComponent {
+  public typealias PropType = (
+    action: Action<UITapGestureRecognizer>,
+    component: Component
+  )
+  
+  public init() {}
+  
+  public func render() -> [Component] {
+    return [self.props().component]
+  }
+  
+  public func view() -> ViewConfiguration? {
+    return ViewConfiguration(
+      view: UIView.self,
+      attributes: [
+        TapAttribute(self.props().action)
+      ])
+  }
+}
