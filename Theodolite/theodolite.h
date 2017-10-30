5b83ab9328a625a73e86c64bbd3450cdf178b2a7
Oliver Clark Rickard <ocrickard@gmail.com>
2017-10-30 19:27:02 -0400

    remove whitespace

--- Theodolite/theodolite.h
+++ Theodolite/theodolite.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double TheodoliteVersionNumber;
 FOUNDATION_EXPORT const unsigned char TheodoliteVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Theodolite/PublicHeader.h>
-
-
