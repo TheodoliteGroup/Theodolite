1700aeaf42dbf3999830517337e0c80a5ad7e705
Oliver Clark Rickard <ocrickard@gmail.com>
2017-11-01 07:51:51 -0400

    Add some basic tests for memoization

---
+++ TheodoliteTests/ComponentMemoizationTests.swift
@@ -0,0 +1,92 @@
+//
+//  ComponentMemoizationTests.swift
+//  TheodoliteTests
+//
+//  Created by Oliver Rickard on 11/1/17.
+//  Copyright © 2017 Oliver Rickard. All rights reserved.
+//
+
+import XCTest
+@testable import Theodolite
+
+class ComponentMemoizationTests: XCTestCase {
+  func test_returningFalse_from_shouldComponentUpdate_doesNotUpdate() {
+    final class TestComponent: TypedComponent {
+      typealias PropType = String
+
+      func shouldComponentUpdate(previous: Component) -> Bool {
+        return false
+      }
+    }
+
+    let c1 = TestComponent {"hello"}
+
+    let scope1 = Scope(listener: nil,
+                       component: c1,
+                       previousScope: nil,
+                       parentIdentifier: ScopeIdentifier(path: []),
+                       stateUpdateMap: [:])
+
+    // Force other props here so the default method would cause it to update
+    let c2 = TestComponent {"argh"}
+
+    let scope2 = Scope(listener: nil,
+                       component: c2,
+                       previousScope: scope1,
+                       parentIdentifier: ScopeIdentifier(path: []),
+                       stateUpdateMap: [:])
+
+    XCTAssert(scope1._component === c1)
+    XCTAssert(scope2._component === c1)
+  }
+
+  func test_notChangingProps_doesNotUpdate() {
+    final class TestComponent: TypedComponent {
+      typealias PropType = String
+    }
+
+    let c1 = TestComponent {"hello"}
+
+    let scope1 = Scope(listener: nil,
+                       component: c1,
+                       previousScope: nil,
+                       parentIdentifier: ScopeIdentifier(path: []),
+                       stateUpdateMap: [:])
+
+    let c2 = TestComponent {"hello"}
+
+    let scope2 = Scope(listener: nil,
+                       component: c2,
+                       previousScope: scope1,
+                       parentIdentifier: ScopeIdentifier(path: []),
+                       stateUpdateMap: [:])
+
+    XCTAssert(scope1._component === c1)
+    XCTAssert(scope2._component === c1)
+  }
+
+  func test_changingProps_updates() {
+    final class TestComponent: TypedComponent {
+      typealias PropType = String
+    }
+
+    let c1 = TestComponent {"hello"}
+
+    let scope1 = Scope(listener: nil,
+                       component: c1,
+                       previousScope: nil,
+                       parentIdentifier: ScopeIdentifier(path: []),
+                       stateUpdateMap: [:])
+
+    let c2 = TestComponent {"argh"}
+
+    let scope2 = Scope(listener: nil,
+                       component: c2,
+                       previousScope: scope1,
+                       parentIdentifier: ScopeIdentifier(path: []),
+                       stateUpdateMap: [:])
+
+    XCTAssert(scope1._component === c1)
+    XCTAssert(scope2._component === c2)
+  }
+}
