c64f4333bfa4076370e6944678bebc5b1e05c8c6
Oliver Clark Rickard <ocrickard@gmail.com>
2017-11-04 18:11:29 -0400

    Add a basic snapshot test for component vc

---
+++ TheodoliteTests/ComponentViewControllerSnapshotTests.swift
@@ -0,0 +1,20 @@
+import FBSnapshotTestCase
+@testable import Theodolite
+
+class ComponentViewControllerSnapshotTests: FBSnapshotTestCase {
+  override func setUp() {
+    super.setUp()
+    recordMode = false
+  }
+
+  func test_componentViewController_singleComponentChild() {
+    let vc = ComponentViewController { () -> Component in
+      return LabelComponent {
+        ("hello",
+         LabelComponent.Options())
+      }
+    }
+    vc.view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    self.FBSnapshotVerifyView(vc.view)
+  }
+}
